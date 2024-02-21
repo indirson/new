aed0c2842efabb61891060cd3f7f9d046362c5e2
Tacoste <chafikmostafa0@gmail.com>
2024-02-21 02:00:06 +0100

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import Contact from './components/Contact.jsx'
+import Accueil from './components/Accueil.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path='/acceuil' element={<Accueil />} />
+        <Route path='/contact' element={<Contact />} />
+        
+      </Routes>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 )
