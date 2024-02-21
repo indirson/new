d108049174a3621b05135924685f136c96bb5ad2
Tacoste <chafikmostafa0@gmail.com>
2024-02-21 01:57:17 +0100

    Update Accueil.jsx

--- Accueil.jsx
+++ Accueil.jsx
@@ -1,15 +1,21 @@
-function Accueil(){
-    return(
-        <>
-        <table>
-            <tr></tr>
-            <tr></tr>
-            <tr></tr>
-            <tr></tr>
-        </table>
-        
-        
+import React from 'react'
 
-     </>
-    )
+function Accueil() {
+
+  return (
+    <>
+      <div className='row pop'>
+        <div className='col-md'><h1 href=''>Acceuil</h1></div>
+        <div className='col-md'><h1 href=''>Contacts</h1></div></div>
+      <div className='container'>
+        <div className='row d-flex justify-content-between'>
+          <h1 className='col-md bg-primary rounded '>title</h1>
+          <h1 className='col-md bg-primary rounded '>product</h1>
+          <h1 className='col-md bg-primary rounded '>price</h1>
+        </div>
+      </div>
+    </>
+  )
 }
+
+export default Accueil
