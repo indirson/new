b68dbbd58dcaf23b0f78047e81add4ce5b78c130
Tacoste <chafikmostafa0@gmail.com>
2024-02-21 01:59:11 +0100

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Accueil from './components/Accueil'
-import Contact from './components/Contact';
+import Accueil from 'Accueil'
+import Contact from 'Contact';
 
 function App() {
   
